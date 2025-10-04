53b35a77fd519935f37a84370c314e58550782ed
AswathyB5 <aswathyb.official@gmail.com>
2025-10-04 22:37:02 +0530

    Task Tracker site Completed

--- script.js
+++ script.js
@@ -1,30 +1,44 @@
 let input = document.getElementById("inputbox");
 let lists = document.getElementById("list-container");
+let editLi = null;
 
 const addtask = () => {
-  if (input == "") {
+  if (input.value === "") {
     alert("please enter task");
-  }else{
-    let li=document.createElement('li')
-    li.innerHTML = `<div class="row ms-3">
-     <div class="col-6">
-        <input type="checkbox" class="me-2">
+  } else if (editLi) {
+    let span = editLi.querySelector("span");
+    span.textContent = input.value;
+    input.value = "";
+    editLi = null;
+  } else {
+    let li = document.createElement("li");
+    li.className = "d-flex align-items-center justify-content-between w-100 mb-1 px-4";
+    li.style.minHeight = "48px";
+    li.innerHTML = `
+      <div class="d-flex align-items-center">
+        <input type="checkbox" class="me-2" onchange="this.nextElementSibling.style.textDecoration = this.checked ? 'line-through' : 'none'">
         <span>${input.value}</span>
       </div>
-      <div class="col-6 mb-1" style="margin-top:-10/"Lpx">
-        <button class="edit-btn btn btn-sm ms-2"><img src="./assets/edit.png" height="34px"></button>
-        <button class="delete-btn btn btn-sm"><img src="./assets/delete.png" height="28px"></button>
-      </div> 
-      </div>`;
-    lists.appendChild(li)
+      <div>
+        <button class="edit-btn btn btn-sm ms-2">
+          <img src="./assets/edit.png" height="34px">
+        </button>
+        <button class="delete-btn btn btn-sm ms-2">
+          <img src="./assets/delete.png" height="30px">
+        </button>
+      </div>
+    `;
+    lists.appendChild(li);
 
-      let deletebtn = li.querySelector(".delete-btn");
-      deletebtn.onclick = () => {
-        li.remove();
-      };
+        let deletebtn = li.querySelector(".delete-btn");
+        deletebtn.onclick = () => {
+          li.remove();
+        };
+    li.querySelector(".edit-btn").onclick = () => {
+      input.value = li.querySelector("span").textContent;
+      editLi = li;
+    };
 
+    input.value = "";
   }
-
 };
-
-
